657add1d8901d280f318a160b0a0fba862d81184
ste1v0 <109760126+ste1v0@users.noreply.github.com>
2022-08-17 20:22:33 +0300

    feat: add while loop, remaining and exit features

--- coffee-machine.js
+++ coffee-machine.js
@@ -26,82 +26,134 @@ let cappuccino = {
   "price": 6
 };
 
-alert(`The coffee machine has:
-400 ml of water
-540 ml of milk
-120 g of coffee beans
-9 disposable cups
-$550 of money`);
+while (true) {
 
-let action = prompt('Write action (buy, fill, take):');
+  let action = prompt('Write action (buy, fill, take, remaining, exit):');
 
-if (action === 'take') {
-  alert(`I gave you ${machine["balance"]}`);
-  machine.balance = 0;
-  alert(`The coffee machine has:
-400 ml of water
-540 ml of milk
-120 g of coffee beans
-9 disposable cups
-${machine["balance"]} of money`);
-} else if (action === 'fill') {
-  let addWater = +prompt('Write how many ml of water you want to add:');
-  let addMilk = +prompt('Write how many ml of milk you want to add:');
-  let addCoffee = +prompt('Write how many grams of coffee beans you want to add:');
-  let addCups = +prompt('Write how many disposable coffee cups you want to add:');
-  
-  machine.water = machine["water"] + addWater;
-  machine.milk = machine["milk"] + addMilk;
-  machine.coffee = machine["coffee"] + addCoffee;
-  machine.cups = machine["cups"] + addCups;
-  
-  alert(`The coffee machine has:
-${machine["water"]} ml of water
-${machine["milk"]} ml of milk
-${machine["coffee"]} g of coffee beans
-${machine["cups"]} disposable cups
-${machine["balance"]} of money`);
-} else if (action === 'buy') {
-  let drink = +prompt('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:');
-  
-  if (drink === 1) { 
-machine.water = machine["water"] - 250;
-machine.coffee = machine["coffee"] - 16;
-machine.balance = machine["balance"] + 4;
-machine.cups = machine["cups"] - 1;
-
-alert(`The coffee machine has:
-${machine["water"]} ml of water
-${machine["milk"]} ml of milk
-${machine["coffee"]} g of coffee beans
-${machine["cups"]} disposable cups
-${machine["balance"]} of money`);
-
-  } else if (drink === 2) {
-machine.water = machine["water"] - 350;
-machine.milk = machine["milk"] - 75;
-machine.coffee = machine["coffee"] - 20;
-machine.balance = machine["balance"] + 7;
-machine.cups = machine["cups"] - 1;
-
-alert(`The coffee machine has:
-${machine["water"]} ml of water
-${machine["milk"]} ml of milk
-${machine["coffee"]} g of coffee beans
-${machine["cups"]} disposable cups
-${machine["balance"]} of money`);
-  } else {
-machine.water = machine["water"] - 200;
-machine.milk = machine["milk"] - 100;
-machine.coffee = machine["coffee"] - 12;
-machine.balance = machine["balance"] + 6;
-machine.cups = machine["cups"] - 1;
-
-alert(`The coffee machine has:
-${machine["water"]} ml of water
-${machine["milk"]} ml of milk
-${machine["coffee"]} g of coffee beans
-${machine["cups"]} disposable cups
-${machine["balance"]} of money`);
+  if (action === 'exit') {
+    break;
   }
+
+  if (action === 'remaining') {
+    alert(`The coffee machine has:
+  ${machine["water"]} ml of water
+  ${machine["milk"]} ml of milk
+  ${machine["coffee"]} g of coffee beans
+  ${machine["cups"]} disposable cups
+  ${machine["balance"]} of money`);
+  continue;
+  }
+
+  if (action === 'take') {
+    alert(`I gave you ${machine["balance"]}`);
+    machine.balance = 0;
+    continue;
+  } 
+  
+  if (action === 'fill') {
+    let addWater = +prompt('Write how many ml of water you want to add:');
+    let addMilk = +prompt('Write how many ml of milk you want to add:');
+    let addCoffee = +prompt('Write how many grams of coffee beans you want to add:');
+    let addCups = +prompt('Write how many disposable coffee cups you want to add:');
+    
+    machine.water = machine.water + addWater;
+    machine.milk = machine.milk + addMilk;
+    machine.coffee = machine.coffee + addCoffee;
+    machine.cups = machine.cups + addCups;
+    continue;
+  } 
+  
+  if (action === 'buy') {
+    
+    let drink = +prompt('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:');
+    
+    if (drink === 1) { 
+    
+    if (machine.water > espresso.water) {
+      if (machine.coffee > espresso.coffee) {
+        if (machine.cups > 0) {
+          alert('I have enough resources, making you a coffee!');
+        } else {
+          alert('Sorry, not enough cups!');
+          continue;
+        }
+      } else {
+        alert('Sorry, not enough coffee beans!');
+        continue;
+      }
+    } else {
+      alert('Sorry, not enough water!');
+      continue;
+    }    
+
+  machine.water = machine.water - espresso.water;
+  machine.coffee = machine.coffee - espresso.coffee;
+  machine.balance = machine.balance + 4;
+  machine.cups = machine.cups - 1;
+  continue;
+  }
+     if (drink === 2) {
+      
+      if (machine.water > latte.water) {
+        if (machine.milk > latte.milk) {
+          if (machine.coffee > latte.coffee) {
+            if (machine.cups > 0) {
+              alert('I have enough resources, making you a coffee!');
+            } else {
+              alert('Sorry, not enough cups!');
+              continue;
+            }
+          } else {
+            alert('Sorry, not enough coffee beans!');
+            continue;
+          }
+        } else {
+          alert('Sorry, not enough milk!');
+          continue;
+        }
+      } else {
+        alert('Sorry, not enough water!');
+        continue;
+      }
+
+      
+  machine.water = machine.water - latte.water;
+  machine.milk = machine.milk - latte.milk;
+  machine.coffee = machine.coffee - latte.coffee;
+  machine.balance = machine.balance + 7;
+  machine.cups = machine.cups - 1;
+  continue;
+} 
+    
+    if (drink === 3) {
+      
+      if (machine.water > cappuccino.water) {
+        if (machine.milk > cappuccino.milk) {
+          if (machine.coffee > cappuccino.coffee) {
+            if (machine.cups > 0) {
+              alert('I have enough resources, making you a coffee!');
+            } else {
+              alert('Sorry, not enough cups!');
+              continue;
+            }
+          } else {
+            alert('Sorry, not enough coffee beans!');
+            continue;
+          }
+        } else {
+          alert('Sorry, not enough milk!');
+          continue;
+        }
+      } else {
+        alert('Sorry, not enough water!');
+        continue;
+      }
+  machine.water = machine.water - cappuccino.water;
+  machine.milk = machine.milk - cappuccino.milk;
+  machine.coffee = machine.coffee - cappuccino.coffee;
+  machine.balance = machine.balance + 6;
+  machine.cups = machine.cups - 1;
+  continue;
+  } 
+}
 }
